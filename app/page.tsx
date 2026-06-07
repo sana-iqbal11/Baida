69b2169b9afda2d6b778b556f25c6676e32e2932
Sana Iqbal <sanaiqbal123876@gmail.com>
2026-06-07 10:55:28 +0400

    home about us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import AboutBaida from "@/components/home/AboutBaida";
 import BestSellers from "@/components/home/BestSeller";
 import Hero from "@/components/home/Hero";
+import TopCategories from "@/components/home/TopCategories";
 
 export default function Home() {
   return (
     <main className="bg-cream">
       <Hero />
       <BestSellers/>
+      {/* <TopCategories/> */}
+      <AboutBaida/>
     </main>
   );
 }
